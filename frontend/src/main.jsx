fdb6881fe9b27fa4ed30f0f1e262016c02b14121
Luiz Guilherme Tristão Gusmão <lgtgusmao@gmail.com>
2025-02-17 18:05:19 -0300

    Adicionando arquivos do projeto

---
+++ frontend/src/main.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+
+import { BrowserRouter } from 'react-router-dom'
+import { theme } from './components/styles/theme.js'
+import { RecoilRoot } from 'recoil'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { SocketContextProvider } from './context/SocketContext.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+
+    <RecoilRoot>
+      <BrowserRouter>
+        <GoogleOAuthProvider clientId="639042207587-uekjbr5e8kkq57tr63t508sgrqm3hbnm.apps.googleusercontent.com">
+          <ChakraProvider theme={theme}>
+            <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+            <SocketContextProvider>
+              <App />
+            </SocketContextProvider>
+          </ChakraProvider>
+        </GoogleOAuthProvider>
+      </BrowserRouter>
+    </RecoilRoot>
+  </React.StrictMode>,
+)
+
